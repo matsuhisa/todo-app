c09024d33cba6332ad1f854cc43592127eb8bcf3
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-08 11:01:44 +0900

    Fix for rubocop

--- spec/views/users/edit.html.haml_spec.rb
+++ spec/views/users/edit.html.haml_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "users/edit", type: :view do
   before(:each) do
-    @user = assign(:user, User.create!(name: "name", email: "email", password_digest: "password")
+    @user = assign(:user, User.create!(name: "name", email: "email", password_digest: "password"))
   end
 
   it "renders the edit user form" do
--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "users/show", type: :view do
   before(:each) do
-    @user = assign(:user, User.create!(name: "name", email: "email", password_digest: "password")
+    @user = assign(:user, User.create!(name: "name", email: "email", password_digest: "password"))
   end
 
   it "renders attributes in <p>" do
