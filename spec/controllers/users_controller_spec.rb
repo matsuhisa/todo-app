4fa876a7a38c8fba5d263eddd5dc229652d29252
Sueyasu Keita <yasukexxx@gmail.com>
2018-06-11 18:09:17 +0900

    Refactor user spec

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,51 +4,64 @@ RSpec.describe UsersController, type: :controller do
   describe "#index" do
     let(:user) { create(:user) }
 
-    before { get :index }
-    it { expect(response).to have_http_status(:success) }
+    subject { get :index }
+
+    it do
+      is_expected.to have_http_status(:success)
+      expect(assigns(:users)).to contain_exactly user
+    end
   end
 
   describe "#show" do
     let(:user) { create(:user) }
 
-    before { get :show, params: { id: user.id } }
-    it { expect(response).to have_http_status(:success) }
+    subject { get :show, params: { id: user.id } }
+
+    it do
+      is_expected.to have_http_status(:success)
+      expect(assigns(:user)).to eq user
+    end
   end
 
   describe "#new" do
-    let(:user) { create(:user) }
+    subject { get :new }
 
-    before { get :new }
-    it { expect(response).to have_http_status(:success) }
+    it do
+      is_expected.to have_http_status(:success)
+      expect(assigns(:user).class).to eq User
+    end
   end
 
   describe "#edit" do
     let(:user) { create(:user) }
 
-    before { get :edit, params: { id: user.id } }
-    it { expect(response).to have_http_status(:success) }
+    subject { get :edit, params: { id: user.id } }
+
+    it do
+      is_expected.to have_http_status(:success)
+      expect(assigns(:user)).to eq user
+    end
   end
 
   describe "#create" do
+    subject { proc { post :create, params: { user: user_params } } }
+
     context "with valid params" do
-      let(:user) { build(:user) }
-      let(:valid_attribute) { { name: user.name, email: user.email, password_digest: user.password_digest } }
+      let(:user_params) { attributes_for :user }
 
       it "creates a new User" do
-        expect {
-          post :create, params: { user: valid_attribute }
-        }.to change { User.count }.from(0).to(1)
-
+        is_expected.to change { User.count }.from(0).to(1)
         expect(response).to redirect_to(User.last)
       end
     end
 
     context "with invalid params" do
-      let(:user) { build(:user) }
-      let(:invalid_attribute) { { name: "", email: user.email, password_digest: user.password_digest } }
+      let(:user_params) { attributes_for :user, name: "" }
 
-      before { post :create, params: { user: invalid_attribute } }
-      it { expect(response).to be_success }
+      it do
+        is_expected.not_to change { User.count }
+        expect(response).to render_template :new
+      end
     end
   end
 end
