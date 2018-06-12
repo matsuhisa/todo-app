73c96f6ff8799bbbfc03b4fd363e79e5a944a8c0
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-12 15:59:35 +0900

    Add rspec of session controller

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,13 +2,32 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
 
-  describe "GET #new" do
-    before do
-      get :new
+  describe "#new" do
+    subject { get :new }
+
+    it { is_expected.to have_http_status(:success) }
+  end
+
+  describe "#create" do
+    subject { post :create, params: { session: user_params } }
+    let!(:user) { create(:user) }
+
+    context 'emailとpasswordが有効なとき' do
+      let(:user_params) { { email: user.email, password: user.password } }
+
+      it { is_expected.to redirect_to user }
     end
 
-    it "returns a success response" do
-      expect(response).to be_successful
+    context 'emailが無効なとき' do
+      let(:user_params) { { email: 'test@example.com', password: user.password } }
+
+      it { is_expected.to render_template :new }
+    end
+
+    context 'passwordが無効なとき' do
+      let(:user_params) { { email: user.email, password: 'foobar' } }
+
+      it { is_expected.to render_template :new }
     end
   end
 end
