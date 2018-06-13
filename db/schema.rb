19467977afbd5ef628466ff0e95614ef471465d3
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-13 18:21:11 +0900

    データベースのmigration

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20180613045409) do
     t.index ["task_id"], name: "index_task_due_dates_on_task_id"
   end
 
-  create_table "tasks", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
     t.integer "user_id", null: false
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20180613045409) do
     t.index ["team_id"], name: "index_team_due_dates_on_team_id"
   end
 
-  create_table "teams", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "teams", force: :cascade do |t|
     t.string "name", null: false
     t.string "color", null: false
     t.datetime "created_at", null: false
@@ -53,7 +53,7 @@ ActiveRecord::Schema.define(version: 20180613045409) do
     t.index ["user_id"], name: "index_user_teams_on_user_id"
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
     t.string "password_digest", null: false
