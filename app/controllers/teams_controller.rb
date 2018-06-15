75c688cb1cdd330587e91f05e647ee6ab77d1634
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-15 09:02:02 +0900

    add vew files of teams

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,50 @@
+class TeamsController < ApplicationController
+  before_action :set_team, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @teams = Team.all
+  end
+
+  def show
+  end
+
+  def new
+    @team = Team.new
+  end
+
+  def edit
+  end
+
+  def create
+    @team = Team.new(team_params)
+
+    if @team.save
+      redirect_to @team, notice: 'team was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @team.update(team_params)
+      redirect_to @team, notice: 'team was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @team.destroy
+    redirect_to teams_url, notice: 'team was successfully destroyed.'
+  end
+
+  private
+
+  def set_team
+    @team = Team.find(params[:id])
+  end
+
+  def team_params
+    params.require(:team).permit(:name, :color)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'top#index'
   resources :users
+  resources :teams
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
