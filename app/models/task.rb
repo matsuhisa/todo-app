a33d6a2d3d941f5bec31463099ec3eedb0e09464
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-19 13:04:03 +0900

    add indents

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,10 @@
 class Task < ApplicationRecord
-validates :title, presence: true, length: { maximum: 100 }
-validates :description, presence: true, length: { maximum: 300 }
-validates :state, presence: true
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :description, presence: true, length: { maximum: 300 }
+  validates :state, presence: true
 
-has_one :user_id
-has_one :team_id
+  has_one :user_id
+  has_one :team_id
 
-enum state: { unfixed: 0, doing: 1, done: 2 }
+  enum state: { unfixed: 0, doing: 1, done: 2 }
 end
