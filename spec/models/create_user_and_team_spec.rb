a9183fb896306aa72c5dab4bbda93cfcd2ae209f
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-19 17:33:31 +0900

    Add spec of CreateUserAndTeam

--- spec/models/create_user_and_team_spec.rb
+++ spec/models/create_user_and_team_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe CreateUserAndTeam, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#save' do
+    let(:user) { build :user, attributes }
+
+    context '保存に成功したとき' do
+      let(:attributes) { {} }
+      let(:decorator) { CreateUserAndTeam.new(user) }
+
+      it { expect(decorator.save).to be_truthy }
+    end
+
+    context '保存に失敗したとき' do
+      let(:attributes) { { name: "" } }
+      let(:decorator) { CreateUserAndTeam.new(user) }
+
+      it { expect(decorator.save).to be_falsey }
+    end
+  end
 end
