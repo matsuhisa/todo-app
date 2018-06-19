f2e2715014ab038e0124d58da806f43915d461fb
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-19 09:38:44 +0900

    Add log_in method into UserController#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,6 +23,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      log_in @user
       redirect_to @user, notice: 'User was successfully created.'
     else
       render :new
