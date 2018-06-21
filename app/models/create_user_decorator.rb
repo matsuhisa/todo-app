1be867ea1acb799c5c92fd2ea60d8f5936cf332f
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-21 15:00:28 +0900

    トランザクション処理を追加

--- app/models/create_user_decorator.rb
+++ app/models/create_user_decorator.rb
@@ -2,19 +2,23 @@ class CreateUserDecorator
   include ActiveModel::Model
   define_model_callbacks :save, only: :after
   after_save :create_team
-  attr_writer :user
+  attr_accessor :user
 
   def save
-    run_callbacks :save do
-      @user.save
+    User.transaction do
+      run_callbacks :save do
+        user.save!
+      end
     end
+    rescue
+      false
   end
 
   private
 
   def create_team
-    team = Team.new({ name: @user.name, color: '#ffffff' })
-    team.users << @user
-    team.save
+    team = Team.new({ name: user.name, color: '#ffffff' })
+    team.users << user
+    team.save!
   end
 end
--- spec/models/create_user_decorator_spec.rb
+++ spec/models/create_user_decorator_spec.rb
@@ -2,21 +2,35 @@ require 'rails_helper'
 
 RSpec.describe CreateUserDecorator, type: :model do
   describe '#save' do
-    subject { user_with_team_create.save }
+    subject { proc { user_with_team_create.save } }
 
     let(:user) { build :user, attributes }
     let(:user_with_team_create) { CreateUserDecorator.new(user: user) }
 
-    context '保存に成功したとき' do
+    context 'ユーザの保存とチームの保存に成功したとき' do
       let(:attributes) { {} }
 
-      it { is_expected.to be_truthy }
+      it do
+        is_expected.to be_truthy
+        is_expected.to change { Team.count }.from(0).to(1).and \
+          change { User.count }.from(0).to(1).and \
+          change { UserTeam.count }.from(0).to(1)
+      end
     end
 
-    context '保存に失敗したとき' do
+    context 'チームの保存に失敗したとき' do
+
+      # it { is_expected.to be_falsey }
+    end
+
+    context 'ユーザの保存に失敗したとき' do
       let(:attributes) { { name: "" } }
 
-      it { is_expected.to be_falsey }
+      it do
+        is_expected.not_to change { User.count }
+        is_expected.not_to change { Team.count }
+        is_expected.not_to change { UserTeam.count }
+      end
     end
   end
 end
