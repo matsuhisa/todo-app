73fe1fb4aad1c2cd81539048a86d4c46d6969669
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-21 10:52:11 +0900

    Refactor CreateUserDecorator spec

--- spec/models/create_user_decorator_spec.rb
+++ spec/models/create_user_decorator_spec.rb
@@ -2,20 +2,21 @@ require 'rails_helper'
 
 RSpec.describe CreateUserDecorator, type: :model do
   describe '#save' do
+    subject { user_with_team_create.save }
+
     let(:user) { build :user, attributes }
+    let(:user_with_team_create) { CreateUserDecorator.new(user: user) }
 
     context '保存に成功したとき' do
       let(:attributes) { {} }
-      let(:decorator) { CreateUserDecorator.new(user: user) }
 
-      it { expect(decorator.save).to be_truthy }
+      it { is_expected.to be_truthy }
     end
 
     context '保存に失敗したとき' do
       let(:attributes) { { name: "" } }
-      let(:decorator) { CreateUserDecorator.new(user: user) }
 
-      it { expect(decorator.save).to be_falsey }
+      it { is_expected.to be_falsey }
     end
   end
 end
