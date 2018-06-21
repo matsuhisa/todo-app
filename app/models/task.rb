7c329d820b6e5bf50c13ffcfcecad7b088aab313
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-21 10:26:05 +0900

    Rename enum of state and Change length of description

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,10 @@
 class Task < ApplicationRecord
   validates :title, presence: true, length: { maximum: 100 }
-  validates :description, presence: true, length: { maximum: 300 }
+  validates :description, presence: true, length: { maximum: 10000 }
   validates :state, presence: true
 
   has_one :user_id
   has_one :team_id
 
-  enum state: { unfixed: 0, doing: 1, done: 2 }
+  enum state: { registered: 0, assigned: 1, completed: 2 }
 end
