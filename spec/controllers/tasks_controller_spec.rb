1d7a1006d5756ba4377fade8a4a56ba94457225b
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-25 11:18:57 +0900

    Fix tasksControllerSpec

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,65 +1,100 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
+  let(:user) { create(:user) }
+
   describe "#index" do
     subject { get :index }
 
-    let(:task) { create(:task) }
+    context 'ログインしている時' do
+      let(:task) { create(:task) }
 
-    it do
-      is_expected.to have_http_status(:ok)
-      expect(assigns(:tasks)).to contain_exactly task
+      before { log_in user }
+
+      it do
+        is_expected.to have_http_status(:ok)
+        expect(assigns(:tasks)).to contain_exactly task
+      end
     end
+
+    context 'ログインしていないとき' do
+      it { is_expected.to redirect_to root_path }
+    end
+
   end
 
   describe "#show" do
     subject { get :show, params: { id: task_id } }
 
-    context 'when id is valid' do
-      let(:task) { create(:task) }
-      let(:task_id) { task.id }
+    let(:task) { create(:task) }
+    let(:task_id) { task.id }
 
-      it do
-        is_expected.to have_http_status(:ok)
-        expect(assigns(:task)).to eq task
+    context 'ログインしている時' do
+      before { log_in user }
+
+      context 'when id is valid' do
+        it do
+          is_expected.to have_http_status(:ok)
+          expect(assigns(:task)).to eq task
+        end
+      end
+
+      context 'when id is invalid' do
+        let(:task_id) { 'aaa' }
+
+        it { expect{subject}.to raise_error ActiveRecord::RecordNotFound }
       end
     end
 
-    context 'when id is invalid' do
-      let(:task_id) { 'aaa' }
-
-      it { expect{subject}.to raise_error ActiveRecord::RecordNotFound }
+    context 'ログインしていない時' do
+      it { is_expected.to redirect_to root_path }
     end
   end
 
   describe "#new" do
     subject { get :new }
 
-    it do
-      is_expected.to have_http_status(:ok)
-      expect(assigns(:task).class).to eq Task
+    context 'ログインしている時' do
+      before { log_in user }
+
+      it do
+        is_expected.to have_http_status(:ok)
+        expect(assigns(:task).class).to eq Task
+      end
+    end
+
+    context 'ログインしていない時' do
+      it { is_expected.to redirect_to root_path }
     end
   end
 
   describe "#create" do
-    subject { proc { post :create, params: { task: task_params } } }
+    subject { post :create, params: { task: task_params } }
 
-    context "with valid params" do
-      let(:task_params) { attributes_for :task }
+    let(:task_params) { attributes_for :task }
 
-      it "creates a new task" do
-        is_expected.to change { Task.count }.from(0).to(1)
-        expect(response).to redirect_to(Task.last)
+    context 'ログインしている時' do
+      before { log_in user }
+
+      context "with valid params" do
+        it "creates a new task" do
+          expect{ subject }.to change { Task.count }.from(0).to(1)
+          is_expected.to redirect_to(Task.last)
+        end
+      end
+
+      context "with invalid params" do
+        let(:task_params) { attributes_for :task, title: "" }
+
+        it do
+          expect{ subject }.not_to change { Task.count }
+          is_expected.to render_template :new
+        end
       end
     end
 
-    context "with invalid params" do
-      let(:task_params) { attributes_for :task, title: "" }
-
-      it do
-        is_expected.not_to change { Task.count }
-        expect(response).to render_template :new
-      end
+    context 'ログインしていない時' do
+      it { is_expected.to redirect_to root_path }
     end
   end
 end
