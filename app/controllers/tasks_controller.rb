530a5647ff2f1d0fe5f56036880945ef0cb911b7
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-25 14:57:59 +0900

    tasksControllerspecに必要なテストを追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,5 @@
 class TasksController < ApplicationController
   before_action :set_task, only: :show
-  before_action :set_user, only: :new
 
   def index
     @tasks = Task.all
@@ -28,10 +27,6 @@ class TasksController < ApplicationController
 
   def set_task
     @task = Task.find(params[:id])
-  end
-
-  def set_user
-    @user = current_user
   end
 
   def task_params
