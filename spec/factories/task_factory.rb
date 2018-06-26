c00f19c3b1abecd35f06fd6b29ca77287a2a0883
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-26 15:56:50 +0900

    必要ない記述をtask_factory.rbから削除

--- spec/factories/task_factory.rb
+++ spec/factories/task_factory.rb
@@ -5,14 +5,5 @@ FactoryBot.define do
     association :user
     association :team
     state 'registered'
-
-    trait :with_user_and_team do
-      before(:create) do |task, _|
-        user = create(:user)
-        team = create(:team, users: [user])
-        task.user = user
-        task.team = team
-      end
-    end
   end
 end
