5307c5bc96b7229c1bab5dc4a0fcc02d511b2e6f
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-26 17:23:10 +0900

    Modify taskController for formobject

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,10 +14,11 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new task_params.merge(user: current_user)
+    @task_form = TaskFrom.new(Task.new, task_form_params.merge(user: current_user))
+    # @task = Task.new task_params.merge(user: current_user)
 
-    if @task.save
-      redirect_to @task, notice: 'task was successfully created.'
+    if @task_form.save
+      redirect_to @task_form.task, notice: 'task was successfully created.'
     else
       render :new
     end
@@ -29,7 +30,7 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
   end
 
-  def task_params
-    params.require(:task).permit(:title, :description, :end_at, :begin_at, :team_id, :state)
+  def task_form_params
+    params.require(:task_form).permit(:title, :description, :end_at, :begin_at, :team_id, :state)
   end
 end
