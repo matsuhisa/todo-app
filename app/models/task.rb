23bee0e98aefc54bcce212ab7e707f200a8341be
haruka-mura <muramoto.haruka@mwed.co.jp>
2018-06-26 14:55:52 +0900

    Add associations

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,6 +3,8 @@ class Task < ApplicationRecord
   validates :description, presence: true, length: { maximum: 10000 }
   validates :state, presence: true
 
+  has_one :task_due_date
+  has_one :completion_date
   belongs_to :user
   belongs_to :team
 
